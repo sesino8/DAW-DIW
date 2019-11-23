61b6728429732b826767cd72b2b4b85e311658a2
sesino8 <tonibermejovalenzuela@gmail.com>
2019-11-23 14:22:18 +0100

    Empiezan a moverse como toca, falta ver los estilos

--- UD03/migracion/js/migracion.js
+++ UD03/migracion/js/migracion.js
@@ -13,31 +13,74 @@ FUNCIONES PERDIDAS
 ^(;,;)^
 
 */
+var hijosPasos;
 
-function startMigration(){
+function startMigration() {
+
+
+    hijosPasos = document.querySelector('steps').children
+
+    for (let index = 0; index < hijosPasos.length; index++) {
+
+        hijosPasos[index].addEventListener("transitionend", transicionar);
+
+    }
+    hijosPasos[0].style.opacity = "0";
+
+
+    hijosPasos[0].style.visibility = "visible";
+
+    hijosPasos[0].style.opacity = "1";
 
     
-var p = document.querySelectorAll('progress'); 
+}
 
-for (let j = 0; j < p.length; j++) {
-for (let index = 1; index <= 10; index++) {
 
-    val = index * 10; 
-    p[j].setAttribute('value', val);
+
+contador = 0;
+var num;
+function transicionar(p) {
+
+    if (contador > 0) {
+        
+    }else{
+        num = this.getAttribute("data-step");
+        hijoSiguiente = parseInt(num) ;
+    }
+
+    console.log(num);
+    hijosPasos[hijoSiguiente].style.visibility = "visible";
+    hijosPasos[hijoSiguiente].style.opacity = "1";
+
     
-}
-}
+    if (num == 0 || num % 3 == 0) {       
+
+      
+        
+    }else if(num == 1 || num % 3 == 1){
+        if (hijosPasos[hijoSiguiente].getAttribute("value") < 100) {
+            contador = contador + 10;
+            hijosPasos[hijoSiguiente].setAttribute('value', contador);
+            transicionar(this);
+        }else{
+            contador = 0;
+        }
+
+    }else{
+
+    }
 
 
 
 }
 
-function init(){
+function init() {
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+    document.querySelector("button").addEventListener("click", startMigration);
+
 }
 
 // Init the environment when all is ready
-window.onload=init;
+window.onload = init;
