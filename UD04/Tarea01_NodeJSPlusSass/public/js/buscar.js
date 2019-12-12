5a58bf72a84a531e209de2b4e60de6a37e231205
sesino8 <tonibermejovalenzuela@gmail.com>
2019-12-12 18:32:26 +0100

    Cambios falleros

--- UD04/Tarea01_NodeJSPlusSass/public/js/buscar.js
+++ UD04/Tarea01_NodeJSPlusSass/public/js/buscar.js
@@ -1,28 +1,36 @@
-const urlRelojes = "http://mapas.valencia.es/lanzadera/opendata/relojes/JSON";
+const urlFallas = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 
 function filtroLetra(elemento){
-    let letra=document.querySelector(`input[name="calle"]`).value;
-    return elemento.properties.emplazamiento.startsWith(letra);
+    let letra=document.querySelector(`input[name="nombre"]`).value;
+    return elemento.properties.nombre.startsWith(letra);
 }
 
 function buscar(){
 
-    const fetchPromesa = fetch(urlRelojes);
+    const fetchPromesa = fetch(urlFallas);
     fetchPromesa.then(response => {
 	return response.json();
     }).then(respuesta =>{
 	const resultado=respuesta.features.filter(filtroLetra);
 
-	let listado=document.createElement("ul");
+	let listado=document.querySelector(".resultados");
 
-	resultado.forEach(reloj=>{
-	    let calleli=document.createElement("li");
-	    calleli.innerHTML=reloj.properties.emplazamiento+" -- ["+reloj.geometry.coordinates+"]";
-	    listado.appendChild(calleli);	    
+	resultado.forEach(falla=>{
+
+		console.log(falla.properties.boceto_i)
+		let celdaResultado = document.createElement("div");
+		celdaResultado.classList.add("celdas");
+		let parrafoResultado = document.createElement("p");
+		parrafoResultado.innerText = falla.properties.nombre;
+		let imagenResultado = document.createElement("img");
+		imagenResultado.src = falla.properties.boceto_i;
+		celdaResultado.appendChild(parrafoResultado);
+		celdaResultado.appendChild(imagenResultado);
+		
+	    listado.appendChild(celdaResultado);	    
 	});
 
-	document.querySelector(".resultados").innerHTML="";
-	document.querySelector(".resultados").appendChild(listado);
+	
     });
 
 }
@@ -30,7 +38,7 @@ function buscar(){
 function init(){
 
     document.querySelector(`input[type="button"]`).addEventListener("click",buscar);    
-    document.querySelector(`input[type="text"]`).addEventListener("input",toUpp);
+    //document.querySelector(`input[type="text"]`).addEventListener("input",toUpp);
 }
 
 
